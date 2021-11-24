2c98105475627c249ea9033c425f448dbcaf4256
SalsaAdelia20 <91942595+SalsaAdelia20@users.noreply.github.com>
2021-11-24 14:48:44 +0700

    Add files via upload

---
+++ RecyclerView2/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.recyclerview
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val INTENT_PARCELABLE = "OBJECT_INTENT"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val superheroList = listOf<Superhero>(
+            Superhero(
+                R.drawable.minion,
+                "minion",
+                "minion"
+            ),
+            Superhero(
+                R.drawable.stitch,
+                "stitch",
+                "stitch"
+            ),
+            Superhero(
+                R.drawable.doraemon,
+                "doraemon",
+                "doraemon"
+            ),
+            Superhero(
+                R.drawable.marsha,
+                "marsa",
+                "marsa"
+            )
+        )
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_hero)
+        recyclerView.layoutManager = LinearLayoutManager( this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = SuperheroAdapter( this,superheroList){
+            val intent = Intent (this, DetailSuperheroActivity::class.java)
+            intent.putExtra(INTENT_PARCELABLE, it)
+            startActivity(intent)
+
+        }
+    }
+}
